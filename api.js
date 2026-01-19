92bbb3c345f099cd5afef1e641a45ad84828335a
link-short <dora.family228@gmail.com>
2026-01-19 14:06:46 +0500

    Update api.js

--- api.js
+++ api.js
@@ -5,3 +5,20 @@ if (bpivDiv) {
 } else {
   console.log("Элемент с классом .bpiv не найден.");
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bpivContainer = document.querySelector('.bpiv');
+
+    if (bpivContainer) {
+        const newElement = document.createElement('p');
+
+        newElement.innerHTML = '<strong><h1>BPiv Captcha</h1></strong>|<div>CAPTCHA<div|';
+
+        bpivContainer.appendChild(newElement);
+
+    } else {
+        console.error("Элемент с классом 'bpiv' не найден.");
+    }
+});
