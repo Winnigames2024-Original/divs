3e0c9c737cb97f37760f87b566a4d0486fbaecae
link-short <dora.family228@gmail.com>
2026-01-19 14:08:04 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,3 @@
-const bpivDiv = document.querySelector('.bpiv');
-
-if (bpivDiv) {
-  bpivDiv.innerHTML = '<h1>BPiv Captcha</h1><p>|>div>CAPTCHA<div|</p>';
-} else {
-  console.log("Элемент с классом .bpiv не найден.");
-}
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const bpivContainer = document.querySelector('.bpiv');
 
