442483184809af86b276f6cebcdcd344aad97989
link-short <dora.family228@gmail.com>
2026-01-19 14:00:55 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,6 @@
-// 1. Находим элемент
 const bpivDiv = document.querySelector('.bpiv');
 
-// 2. Проверяем, существует ли элемент
 if (bpivDiv) {
-  // 3. Вставляем HTML-код
   bpivDiv.innerHTML = '<h1>BPiv Captcha</h1><p>|>div>CAPTCHA<div|</p>';
 } else {
   console.log("Элемент с классом .bpiv не найден.");
